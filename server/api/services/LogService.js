6f8fb11c9562a8cbabcd41047f161ccdbdd7fcee
Roy Talyosef <roy@productionmap.com>
2017-11-05 14:33:52 +0200

    PMG-114 Move executions and logs outside of map entity Created basic create functions for Log model

---
+++ server/api/services/LogService.js
@@ -0,0 +1,43 @@
+function modelFor(instance) {
+    /* the function get an instance and check which model its intance of. then, return the class name or null if not found  */
+    var model;
+    for (var key in sails.models) {
+        model = sails.models[key];
+        if ( instance instanceof model._model.__bindData__[0] ) {
+            break;
+        }
+        model = undefined;
+    }
+    if (model) {
+        return key.charAt(0).toUpperCase() + key.slice(1);
+    } else {
+        return ;
+    }
+  }
+
+module.exports = {
+
+    createLog: function(msg, instance, reason, status) {
+        console.log("Creating log");
+        let obj = { message: msg, objectId: instance.id, model: modelFor(instance), reason: reason, status: status };
+        return Log.create(obj).catch((error)=> console.log(error));
+    },
+
+    getLogsForInstance: function(instance) {
+        /* this function get an instance and returns all the logs for this instance */
+        return Log.find({ objectId: instance.id, model: modelFor(instance) });
+    },
+
+    fail: function(msg, instance, reason) {
+        return LogService.createLog(msg, instance, reason, "error");
+    },
+
+    success: function(msg, instance, reason) {
+        return LogService.createLog(msg, instance, reason, "success");
+    },
+
+    info: function(msg, instance, reason) {
+        return LogService.createLog(msg, instance, reason, "info");
+    }
+
+ }
