5eb18fd0595d52c753dcf72a4176b6723690b387
Roy Talyosef <roy@productionmap.com>
2017-11-16 19:10:10 +0200

    PMG-18 Trigger Panel: Missing "Edit" Pencil Icon Added trigger edit

--- client/src/app/admin-panel/triggers/add-trigger/add-trigger.component.ts
+++ client/src/app/admin-panel/triggers/add-trigger/add-trigger.component.ts
@@ -16,23 +16,18 @@ import * as _ from 'lodash';
   providers: [TriggerService]
 })
 export class AddTriggerComponentWindow implements OnInit, OnDestroy {
-  trigger: any = {
-    project: {
-      maps: []
-    },
-    type: {
-      params: []
-    }
-  };
   
+  
+  @Input() trigger: any;
   types: any[] = [];
   projects: any[] = [];
   selectedProject: any;
   selectedMap: any;
   projectMaps: any[];
-  @Input()triggersList: any[];
   mapsReq: any;
   projectsReq: any;
+  title: string;
+  update: boolean;
 
   constructor(public dialog: NgbActiveModal, public modalService: NgbModal, private constsService: ConstsService,
               private authService: AuthenticationService, private projectService: ProjectService, private triggerService: TriggerService) {
@@ -43,11 +38,44 @@ export class AddTriggerComponentWindow implements OnInit, OnDestroy {
   ngOnInit() {
     this.projectsReq = this.projectService.getJstreeProjectsByUser(this.authService.getCurrentUser().id).subscribe((projects) => {
         this.projects = projects;
-        console.log(projects);
       },
       (error) => {
         console.log(error);
     });
+
+    console.log(">>", this.trigger);
+
+    if (this.trigger) {
+      this.update = true;
+      this.title = "Edit trigger";
+      this.selectedProject = this.trigger.project;
+      let typeIndex = _.findIndex(this.types, (type) => {
+        return type.viewName === this.trigger.type
+      });
+      this.trigger.type = this.types[typeIndex];
+      this.trigger.type.params = _.toArray(this.trigger.params);
+      if (this.selectedProject) {
+        this.onSelectProject().then(() => {
+          let mapIndex = _.findIndex(this.projectMaps, (map) => {
+            return map.id === this.trigger.map.id
+          })
+          this.selectedMap = this.projectMaps[mapIndex];
+        });
+      }
+      
+    } else {
+      this.update = false;
+      this.trigger = "Add Trigger";
+      this.trigger = {
+        project: {
+          maps: []
+        },
+        type: {
+          params: []
+        }
+      };
+    }
+    console.log("^^", this.selectedMap);
   }
 
   ngOnDestroy() {
@@ -58,14 +86,19 @@ export class AddTriggerComponentWindow implements OnInit, OnDestroy {
   }
 
   onSelectProject() {
-    this.selectedMap = null;
-    this.mapsReq = this.projectService.getProjectMaps(this.selectedProject.id).subscribe((maps) => {
-      this.projectMaps = maps;
+    return new Promise((res, rej) => {
+      this.selectedMap = null;
+      this.mapsReq = this.projectService.getProjectMaps(this.selectedProject.id).subscribe((maps) => {
+        this.projectMaps = maps;
+        res();
+      })
     })
+    // this.selectedMap = null;
+    
   }
 
   saveTrigger(trigger) {
-    console.log(this.selectedMap);
+    console.log(trigger);
     trigger.params = {};
     
     for (let i = 0; i < trigger.type.params.length; i++) {
@@ -73,14 +106,17 @@ export class AddTriggerComponentWindow implements OnInit, OnDestroy {
       trigger.params[param.name] = param;
     }
     trigger.type = trigger.type.viewName;
-    trigger.map = this.selectedMap.id;
+    trigger.map = this.selectedMap? this.selectedMap.id: null;
     trigger.project = this.selectedProject;
-
-    this.triggerService.add(trigger).subscribe((res) => {
-      trigger.map = {name: this.selectedMap.name}
-      this.triggersList.push(trigger);
-    });
-    this.dialog.close();
+    console.log(trigger);
+    if (this.update) {
+      this.triggerService.update(trigger).subscribe();
+    } else {
+      this.triggerService.add(trigger).subscribe();
+    }
+    trigger.map = {name: this.selectedMap.name}
+    this.dialog.close(trigger);
+    
   }
 
   closeWindow() {
