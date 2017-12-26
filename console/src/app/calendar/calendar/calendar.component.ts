b45cb409c4a6c008a937c12d2c7c14d22e45f0f5
Roy Talyosef <roy@productionmap.com>
2017-12-26 10:17:18 +0200

    PMG-174 Pass calendar to new design Remove events mock

--- console/src/app/calendar/calendar/calendar.component.ts
+++ console/src/app/calendar/calendar/calendar.component.ts
@@ -42,39 +42,6 @@ export class CalendarComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.events = [/* mock event data */
-      {
-        start: subDays(startOfDay(new Date()), 1),
-        end: addDays(new Date(), 1),
-        title: 'A 3 day event',
-        color: colors.red,
-        // actions: this.actions
-      },
-      {
-        start: startOfDay(new Date()),
-        title: 'An event with no end date',
-        color: colors.yellow,
-        // actions: this.actions
-      },
-      {
-        start: subDays(endOfMonth(new Date()), 3),
-        end: addDays(endOfMonth(new Date()), 3),
-        title: 'A long event that spans 2 months',
-        color: colors.blue
-      },
-      {
-        start: addHours(startOfDay(new Date()), 2),
-        end: new Date(),
-        title: 'A draggable and resizable event',
-        color: colors.yellow,
-        // actions: this.actions,
-        resizable: {
-          beforeStart: true,
-          afterEnd: true
-        },
-        draggable: true
-      }
-    ];
   }
 
   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
