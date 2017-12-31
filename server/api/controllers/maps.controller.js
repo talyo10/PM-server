4c2ecb38908fe88bd274dc777971e345de4fac8c
Roy Talyosef <roy@productionmap.com>
2017-12-31 16:00:11 +0200

    PMG-194 Missing notifications Added notifications in controllers

--- server/api/controllers/maps.controller.js
+++ server/api/controllers/maps.controller.js
@@ -13,10 +13,13 @@ module.exports = {
         }
         mapsService.create(req.body).then((map) => {
             projectsService.addMap(req.body.project, map._id).then(() => {
+                req.io.emit('notification', { title: 'Map created', message: `Map ${map.name} was created`, type: 'success' });
                 res.json(map);
             });
         }).catch((error) => {
             console.log("Error creating map: ", error);
+            req.io.emit('notification', { title: 'Error', message: `Error creating map. Please try again`, type: 'error' });
+
             res.status(500).send(error);
         })
     },
@@ -26,6 +29,7 @@ module.exports = {
             return res.status(200).json(map);
         }).catch((error) => {
             console.log("Error finding map: ", error);
+            req.io.emit('notification', { title: 'Whoops...', message: `Error finding map`, type: 'error' });
             return res.status(500).json(error);
         });
     },
@@ -35,6 +39,8 @@ module.exports = {
             return res.status(200).json(maps);
         }).catch((error) => {
             console.log("Error filtering maps: ", error);
+            req.io.emit('notification', { title: 'Whoops...', message: `Error filtering maps`, type: 'error' });
+
             return res.status(500).json(error);
         });
     },
@@ -54,6 +60,8 @@ module.exports = {
             return res.send('');
         }).catch((error) => {
             console.log("Error updating map: ", error);
+            req.io.emit('notification', { title: 'Whoops...', message: `Error updating map`, type: 'error' });
+
             return res.status(500).json(error);
         });
     },
@@ -64,9 +72,12 @@ module.exports = {
         let mapId = req.params.id;
         req.body.map = mapId;
         mapsService.createStructure(req.body).then(structure => {
+            req.io.emit('notification', { title: 'Saved', message: `Map saved successfully`, type: 'success' });
             res.json(structure)
         }).catch((error) => {
             console.log("Error creating map structure: ", error);
+            req.io.emit('notification', { title: 'Whoops...', message: `Error saving map structure`, type: 'error' });
+
             res.status(500).send(error);
         })
     },
@@ -86,6 +97,8 @@ module.exports = {
             res.json(r);
         }).catch(error => {
             console.log("Error executing map: ", error);
+            req.io.emit('notification', { title: 'Whoops...', message: `Error executing map`, type: 'error' });
+
             return res.status(500).json(error);
         });
     },
@@ -104,6 +117,8 @@ module.exports = {
             res.json(results);
         }).catch(error => {
             console.log("Error getting execution results: ", error);
+            req.io.emit('notification', { title: 'Whoops...', message: `Error getting execution results`, type: 'error' });
+
             return res.status(500).json(error);
         });
     },
@@ -112,6 +127,8 @@ module.exports = {
     /* create trigger */
     triggerCreate: (req, res) => {
         triggersService.create(req.params.id, req.body).then(trigger => {
+            req.io.emit('notification', { title: 'Trigger saved', message: `${trigger.name} saved successfully`, type: 'success' });
+
             return res.json(trigger);
         }).catch((error) => {
             console.log("Error getting map's triggers: ", error);
@@ -121,8 +138,11 @@ module.exports = {
     /* delete a trigger */
     triggerDelete: (req, res) => {
         triggersService.delete(req.params.triggerId).then(() => {
+            req.io.emit('notification', { title: 'Trigger deleted', message: `${trigger.name} saved successfully`, type: 'success' });
+
             return res.send("OK");
         }).catch((error) => {
+            req.io.emit('notification', { title: 'Error deleting', message: `We couldn't delete this trigger`, type: 'error' });
             console.log("Error getting map's triggers: ", error);
             return res.status(500).json(error);
         });
@@ -139,6 +159,8 @@ module.exports = {
     /* update a trigger */
     triggerUpdate: (req, res) => {
         triggersService.update(req.params.triggerId, req.body).then(trigger => {
+            req.io.emit('notification', { title: 'Trigger saved', message: `${trigger.name} saved successfully`, type: 'success' });
+
             return res.json(trigger);
         }).catch((error) => {
             console.log("Error updating triggers: ", error);
