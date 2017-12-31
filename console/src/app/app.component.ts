425445a357cbb4ebeebba99487122620a9a12ea4
Roy Talyosef <roy@productionmap.com>
2017-12-31 15:59:54 +0200

    PMG-194 Missing notifications Added toasty module and added notification to app.component

--- console/src/app/app.component.ts
+++ console/src/app/app.component.ts
@@ -1,17 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SocketService } from './shared/socket.service';
+import { Subscription } from 'rxjs/Subscription';
+
+import { ToastyService, ToastyConfig, ToastOptions, ToastData } from 'ng2-toasty';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   search: boolean = false;
+  notificationSubscription: Subscription;
 
+  constructor(private socketService: SocketService, private toastyService: ToastyService, private toastyConfig: ToastyConfig) {
+    this.toastyConfig.theme = 'material';
+    this.toastyConfig.position = "bottom-center";
+  }
+
+  ngOnInit() {
+    this.notificationSubscription = this.socketService.getNotificationAsObservable().subscribe(notification => {
+      const toastOptions: ToastOptions = {
+        title: notification.title,
+        msg: notification.message,
+        showClose: true,
+        timeout: 5000
+      };
+      switch (notification.type) {
+        case 'default':
+          return this.toastyService.default(toastOptions);
+        case 'info':
+          return this.toastyService.info(toastOptions);
+        case 'success':
+          return this.toastyService.success(toastOptions);
+        case 'wait':
+          return this.toastyService.wait(toastOptions);
+        case 'error':
+          return this.toastyService.error(toastOptions);
+        case 'warning':
+          return this.toastyService.warning(toastOptions);
+      }
+    });
+  }
 
   toggleSearch() {
     this.search = !this.search;
   }
+
 
 }
