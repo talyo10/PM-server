867c0b864e72bb7e0eddd03def713172173d87cc
Roy Talyosef <roy@productionmap.com>
2018-01-02 13:00:50 +0200

    PMG-201 Add Paging to grid added primeng data table

--- console/src/app/projects/projects-list/projects-list.component.ts
+++ console/src/app/projects/projects-list/projects-list.component.ts
@@ -8,18 +8,37 @@ import { Project } from '../models/project.model';
   styleUrls: ['./projects-list.component.scss']
 })
 export class ProjectsListComponent implements OnInit {
-  projects: [Project];
+  projects: Project[];
   projectsReq: any;
+  featuredReq: any;
   filterTerm: string;
+  page: number = 1;
+  resultCount: number;
 
   constructor(private projectsService: ProjectsService) {
   }
 
   ngOnInit() {
-    this.projectsReq = this.projectsService.list().subscribe(projects => {
-      this.projects = projects;
-      this.projects.map(project => Object.assign({}, project, { expanded: false }));
+    this.projectsReq = this.projectsService.filter(null, null, this.page).subscribe(data => {
+      this.projects = data.items;
+      this.resultCount = data.totalCount;
+    });
+  }
+
+  loadProjectLazy(event) {
+    let fields, page, sort;
+    if (event) {
+      fields = event.filters || null;
+      page = event.first / 5 + 1;
+      if (event.sortField) {
+        sort = event.sortOrder === -1 ? '-' + event.sortField : event.sortField;
+      }
+    }
+    this.projectsService.filter(fields, sort, page, this.filterTerm).subscribe(data => {
+      this.projects = data.items;
+      this.resultCount = data.totalCount;
     });
   }
 
 }
+
--- console/src/app/projects/projects.service.ts
+++ console/src/app/projects/projects.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpParams } from '@angular/common/http';
 
-import { environment } from "../../environments/environment";
-import { Project } from "./models/project.model";
+import { environment } from '../../environments/environment';
+import { Project } from './models/project.model';
 
 
 const serverUrl = environment.serverUrl;
@@ -13,24 +13,39 @@ export class ProjectsService {
   constructor(private http: HttpClient) {
   }
 
- create(project) {
-    return this.http.post<Project>(serverUrl + "api/projects/create", project);
- }
-
-  detail(projectId) {
-    return this.http.get<Project>(serverUrl + "api/projects/" + projectId);
+  create(project) {
+    return this.http.post<Project>(serverUrl + 'api/projects/create', project);
   }
 
-  filter(query) {
-    let params = new HttpParams().set('q', query);
-    return this.http.get<[Project]>(serverUrl + "api/maps/", { params: params });
+  detail(projectId) {
+    return this.http.get<Project>(serverUrl + 'api/projects/' + projectId);
+  }
+
+  filter(fields?: any, sort?: string, page?: number, globalFilter?: string) {
+    console.log(fields, sort, page);
+    let params = new HttpParams();
+    if (fields) {
+      Object.keys(fields).map(key => {
+        params = params.set(`fields[${key}]`, fields[key]);
+      });
+    }
+    if (sort) {
+      params = params.set('sort', sort);
+    }
+    if (page) {
+      params = params.set('page', page.toString());
+    }
+    if (globalFilter) {
+      params = params.set('globalFilter', globalFilter);
+    }
+    return this.http.get<{ totalCount: number, items: Project[] }>(`${serverUrl}api/projects`, { params: params });
   }
 
   list() {
-    return this.http.get<[Project]>(serverUrl + "api/projects");
+    return this.http.get<[Project]>(serverUrl + 'api/projects');
   }
 
   update(projectId, project) {
-    return this.http.put<Project>(serverUrl + "api/projects/" + projectId + "/update", project);
+    return this.http.put<Project>(serverUrl + 'api/projects/' + projectId + '/update', project);
   }
 }
